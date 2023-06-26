e5a97423b3d37dca66cbff22c0146d5167aed977
Ethan Bradley <ethan.m.bradley@gmail.com>
2023-06-25 23:22:38 -0400

    added stress test for maximum pairwise product

--- maximum_pairwise_product/src/main.rs
+++ maximum_pairwise_product/src/main.rs
@@ -44,22 +44,37 @@ mod tests {
     use super::maximum_pairwise_product;
     use rand::{distributions::Uniform, thread_rng, Rng};
 
+    fn naive_maximum_pairwise_product(nums: Box<[u64]>) -> u64 {
+        let mut max = 0;
+        for (i, a) in nums.iter().enumerate() {
+            for (j, b) in nums.iter().enumerate() {
+                if i == j {
+                    continue;
+                }
+                if a * b > max {
+                    max = a * b;
+                }
+            }
+        }
+        max
+    }
+
     #[test]
-    fn small_test() {
+    fn tiny_test() {
         let nums = [1, 2, 3];
         let answer = 6;
         assert_eq!(maximum_pairwise_product(nums.iter().copied()), answer)
     }
 
     #[test]
-    fn medium_test() {
+    fn small_test() {
         let nums = [7, 5, 14, 2, 8, 8, 10, 1, 2, 3];
         let answer = 140;
         assert_eq!(maximum_pairwise_product(nums.iter().copied()), answer)
     }
 
     #[test]
-    fn big_test() {
+    fn time_test() {
         let distribution = Uniform::new_inclusive(0, 200000);
         let mut rng = thread_rng();
         let count = 200000;
@@ -69,8 +84,21 @@ mod tests {
         let elapsed = start.elapsed().as_millis();
         assert!(
             elapsed < 1000,
-            "solution took too long to run ({} ms, maximum 1 s)",
+            "solution took too long to run ({} ms)",
             elapsed
         )
+    }
+
+    #[test]
+    fn stress_test() {
+        let distribution = Uniform::new_inclusive(2, 10);
+        let mut rng = thread_rng();
+        for _ in 0..10000 {
+            let count = rng.sample(distribution);
+            let nums: Box<[u64]> = (0..count).map(|_| rng.sample(distribution)).collect();
+            let fast = maximum_pairwise_product(nums.iter().copied());
+            let naive = naive_maximum_pairwise_product(nums);
+            assert_eq!(naive, fast)
+        }
     }
 }
