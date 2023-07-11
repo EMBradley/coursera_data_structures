a5f5331964787f154153ba2de8f1926ca758410e
Ethan Bradley <ethan.m.bradley@gmail.com>
2023-07-11 12:39:51 -0400

    improved binary search with dups from O(sqrt(n)) to O(log(n))

--- divide_and_conquer/src/bin/binary_search_duplicates.rs
+++ divide_and_conquer/src/bin/binary_search_duplicates.rs
@@ -1,13 +1,13 @@
-use std::{cmp::Ordering, io};
+use std::io;
 
 fn main() {
     let n = read_number();
     let list = read_list(n);
     let m = read_number();
     let queries = read_list(m);
-    let results: Vec<String> = queries
-        .into_iter()
-        .map(|q| match binary_search_duplicates(&list, &q) {
+    let results: Box<[String]> = queries
+        .iter()
+        .map(|q| match binary_search_duplicates(&list, q) {
             Some(i) => i.to_string(),
             None => "-1".to_string(),
         })
@@ -23,58 +23,34 @@ fn read_number() -> usize {
     input.trim().parse().expect("failed to parse input")
 }
 
-fn read_list(length: usize) -> Vec<u32> {
-    let mut vec = Vec::with_capacity(length);
+fn read_list(length: usize) -> Box<[u32]> {
+    let mut list = vec![0; length].into_boxed_slice();
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("failed to read input");
-    for n in input.split(' ') {
-        let n = n.trim().parse().expect("failed to parse input");
-        vec.push(n);
+    for (i, n) in input.split(' ').enumerate() {
+        list[i] = n.trim().parse().expect("failed to parse input");
     }
-    vec
+    list
 }
 
 fn binary_search_duplicates<T: Ord>(list: &[T], query: &T) -> Option<usize> {
-    let mut i = binary_search(list, query)?;
-
-    // We use jumps of length sqrt(n) to avoid doing a linear search over a fixed portion of the
-    // given list. If we did a linear search to get from *an* index with the desired value to the
-    // first one, then in the worst case, binary search would find the element halfway through the
-    // list and we would search back all the way to the beginning to see that the list started with
-    // the desired value. Then the algorithm as a whole would be O(n/2) = O(n).
-    // Jumping by steps of sqrt(n) means that we do at most O(sqrt(n)) jumps, then do a linear
-    // search over a chunk of length O(sqrt(n)). This makes the algorithm as a whole O(sqrt(n)).
-    let sqrt_len = (list.len() as f64).sqrt() as usize + 1;
-    while list[i] >= *query {
-        if i < sqrt_len {
-            i = 0;
-            break;
-        }
-        i -= sqrt_len;
-    }
-    if list[i] > *query {
-        return None;
-    }
-    while list[i] < *query {
-        i += 1;
-    }
-    Some(i)
-}
-
-fn binary_search<T: Ord>(list: &[T], query: &T) -> Option<usize> {
     let mut left = 0;
     let mut right = list.len();
     while left < right {
         let m = left + (right - left) / 2;
-        match list[m].cmp(query) {
-            Ordering::Less => left = m + 1,
-            Ordering::Greater => right = m,
-            Ordering::Equal => return Some(m),
+        if list[m] < *query {
+            left = m + 1;
+        } else {
+            right = m;
         }
     }
-    None
+    if list.get(right)? == query {
+        Some(right)
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -93,9 +69,27 @@ mod tests {
         }
     }
     #[test]
+    fn empty_list() {
+        let list = [];
+        let query = 1;
+        assert_eq!(binary_search_duplicates(&list, &query), None);
+    }
+    #[test]
+    fn single_item_with_query() {
+        let list = [0];
+        let query = 0;
+        assert_eq!(binary_search_duplicates(&list, &query), Some(0));
+    }
+    #[test]
+    fn single_item_without_query() {
+        let list = [0];
+        let query = 1;
+        assert_eq!(binary_search_duplicates(&list, &query), None);
+    }
+    #[test]
     fn stress_test() {
         let mut rng = thread_rng();
-        let mut list = [0u8; 4096];
+        let mut list = [0u8; 4096]; // using u8 to guarantee duplicates
         let test_count = 100;
         let query_count = 100;
         for _ in 0..test_count {
